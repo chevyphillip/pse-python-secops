126fd765335d9fd597a593e0ecdf35e0bdda3300
Chevy Phillip <chevonmdphillip@gmail.com>
2023-10-31 21:51:56 -0400

    feat: Add black code formatter
    This commit adds the black code formatter to the project. Black is an uncompromising Python code formatter that provides speed, determinism, and freedom from pycodestyle nagging about formatting. It reformats entire files in place and produces the smallest diffs possible, making code review faster. The black version added is 23.10.1.
    
    For more information on how to install and use Black, please refer to the documentation.
    
    Co-authored-by: Łukasz Langa <lukasz@langa.pl>

--- main.py
+++ main.py
@@ -1,5 +1,28 @@
+import requests
+import json
+
+
+# TODO: Parse vuln.json from the inital Snyk scan
+def vuln_data_parser():
+    with open("vuln.json") as json_file:
+        data = json.load(json_file)
+        for p in data["vulnerabilities"]:
+            print("Title: " + p["title"])
+            print("Package: " + p["packageName"])
+            print("Version: " + p["version"])
+            print("ID: " + p["id"])
+            print("")
+
+
+# TODO: If Critical security issues are detected as a result of the Snyk test create a GitHub Issue that contains a list of the security issues detected. The issue id, title, package and version should be included for each entry
+# TODO: If no Critical security issues are detected, the GitHub Issue should still be created, but display "No Security Issues Found"
+# TODO: Generate github issues for each of the security issues detected
+# TODO: Check for errors and handle them gracefully
+# TODO: Add logging to slack - nice to have
+
+
 def main():
-    print("Hello, Chevy!")
+    vuln_data_parser()
 
 
 if __name__ == "__main__":
