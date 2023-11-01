0643935ced888a16757482286e4d4476c4216957
Chevy Phillip <chevonmdphillip@gmail.com>
2023-10-31 23:02:55 -0400

    Fix authorization typo and remove unused import
    - Fix a typo in the Authorization header key
    - Remove an unused import statement

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import requests
 import json
 import os
-import env
 
 
 # TODO: Parse vuln.json from the inital Snyk scan
@@ -12,7 +11,7 @@ def get_list_of_current_github_issues():
     github_issue_params = {"owner": "chevyphillip", "repo": "nodejs-goof"}
     headers = {
         "Accept": "Accept: application/vnd.github+json",
-        "Authorzation": "Bearer " + {github_token},
+        "Authorzation": "Bearer " + github_token,
         "X-GitHub-Api-Version": "2022-11-28",
     }
     url = f"https://api.github.com/repos/{github_issue_params['owner']}/{github_issue_params['repo']}/issues"
