fa3d4a4841f2a48e42377ed07a9452a28d70b399
Chevy Phillip <chevonmdphillip@gmail.com>
2023-11-01 12:16:39 -0400

    Refactor vuln_data_parser to handle critical security issues
    The code in vuln_data_parser.py has been modified to include a check for critical security issues. If a vulnerability is flagged as "critical", the relevant details are printed. Otherwise, the message "No Security Issues Found" is displayed. This change improves the handling of security vulnerabilities in the code.

--- vuln_data_parser.py
+++ vuln_data_parser.py
@@ -5,9 +5,13 @@ def vuln_data_parser():
     with open("vuln.json") as json_file:
         data = json.load(json_file)
         for p in data["vulnerabilities"]:
-            print("")
-            print("ID: " + p["id"])
-            print("Title: " + p["title"])
-            print("Package: " + p["packageName"])
-            print("Version: " + p["version"])
-            print("")
+            if data["vulnerabilities"][0]["severity"] == "critical":
+                print("")
+                print("Critical security issues detected!")
+                print("ID: " + p["id"])
+                print("Title: " + p["title"])
+                print("Package: " + p["packageName"])
+                print("Version: " + p["version"])
+                print("")
+            else:
+                print("No Security Issues Found")
