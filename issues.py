fd8c474d1cbfbf14c7aaee0f8a397ed4f4d7bd67
Chevy Phillip <chevonmdphillip@gmail.com>
2023-11-01 13:08:23 -0400

    Refactor code to create GitHub issues for security vulnerabilities
    - Refactored `create_github_issue` function in `issues.py` to include logic for creating GitHub issues based on severity of security vulnerabilities
    - Added `parse_vuln_data` function in `issues.py` to parse vulnerability data from "vuln.json" file
    - Removed unused import statements and deleted `vuln_data_parser.py`
    - Updated `main.py` to call the refactored `create_github_issue` function instead of the deprecated functions

--- issues.py
+++ issues.py
@@ -1,12 +1,48 @@
 import requests
 from helper import *
+import json
 
 
-def get_list_of_current_github_issues():
-    github_issue_params = {"owner": "chevyphillip", "repo": "nodejs-goof"}
+import requests
+import json
+from helper import get_github_auth
+
+
+def parse_vuln_data():
+    try:
+        with open("vuln.json", "r") as f:
+            data = json.load(f)
+            return data
+    except Exception as e:
+        print(e)
+
+
+def create_github_issue():
+    github_repo = {"owner": "chevyphillip", "repo": "nodejs-goof"}
+    url = f"https://api.github.com/repos/{github_repo['owner']}/{github_repo['repo']}/issues"
     headers = get_github_auth()
-    url = f"https://api.github.com/repos/{github_issue_params['owner']}/{github_issue_params['repo']}/issues"
-    r = requests.get(url, headers=headers)
+
+    vuln_data = parse_vuln_data()
+
+    id, title, package, version = (
+        vuln_data["id"],
+        vuln_data["title"],
+        vuln_data["package"],
+        vuln_data["version"],
+    )
+
+    severity = vuln_data["severity"]
+
+    if severity == "critical":
+        title = f"[CRITICAL SECURITY ISSUE DETECTED] - {title}"
+        body = f"ID: {id}, Title: {title}, Package: {package}, Version: {version}"
+    else:
+        title = "[No Security Issues Found]"
+        body = "No Security Issues Found"
+
+    payload = {"title": title, "body": body}
+
+    r = requests.post(url, headers=headers, json=payload)
     print(r.status_code)
     print(r.json())
-    print("It Works!")
+    print("Issues Created!")
--- main.py
+++ main.py
@@ -1,17 +1,9 @@
 from helper import *
-from issues import get_list_of_current_github_issues
-from vuln_data_parser import vuln_data_parser
-
-# TODO: If Critical security issues are detected as a result of the Snyk test create a GitHub Issue that contains a list of the security issues detected. The issue id, title, package and version should be included for each entry
-# TODO: If no Critical security issues are detected, the GitHub Issue should still be created, but display "No Security Issues Found"
-# TODO: Generate github issues for each of the security issues detected
-# TODO: Check for errors and handle them gracefully
-# TODO: Add logging to slack - nice to have
+from issues import create_github_issue
 
 
 def main():
-    vuln_data_parser()
-    get_list_of_current_github_issues()
+    create_github_issue()
 
 
 if __name__ == "__main__":
