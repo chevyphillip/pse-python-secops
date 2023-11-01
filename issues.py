4a8ee0bf9021e306435e12efab502fdc2a75699a
Chevy Phillip <chevonmdphillip@gmail.com>
2023-11-01 14:02:53 -0400

    Refactor create_github_issue() to handle updated vuln_data structure
    The code in create_github_issue() has been modified to accommodate changes in the structure of the vuln_data variable. Instead of accessing the severity field using index notation, it now accesses it directly from the vulnerability object. This change ensures that critical vulnerabilities are correctly identified and processed when creating GitHub issues.

--- issues.py
+++ issues.py
@@ -20,8 +20,8 @@ def create_github_issue():
 
     vuln_data = parse_vuln_data()
 
-    for vuln_data_payload in vuln_data:
-        if vuln_data_payload[0]["severity"] == "critical":
+    for vuln_data_payload in vuln_data["vulnerabilities"]:
+        if vuln_data_payload["severity"] == "critical":
             title = f"Critical Vulnerability Found in {vuln_data_payload['title']}"
             body = f"[X] - A critical vulnerability was found in {vuln_data_payload['packageName']}\nVersion: {vuln_data_payload['version']}\nSnyk ID: {vuln_data_payload['id']}."
         else:
