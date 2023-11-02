7fe18034b59505882dada1c8b254376dd327540a
Chevy Phillip <chevonmdphillip@gmail.com>
2023-11-02 14:00:19 -0400

    Add import statement for 're' module in issues.py and implement a function to delete duplicate GitHub issues based on Snyk ID.

--- issues.py
+++ issues.py
@@ -1,3 +1,4 @@
+import re
 import requests
 import json
 from helper import get_github_auth
@@ -24,14 +25,44 @@ def create_github_issue():
         if vuln_data_payload["severity"] == "critical":
             title = f"Critical Vulnerability Found in {vuln_data_payload['title']}"
             body = f"[X] - A critical vulnerability was found in {vuln_data_payload['packageName']}\nVersion: {vuln_data_payload['version']}\nSnyk ID: {vuln_data_payload['id']}."
+        elif vuln_data_payload["severity"] != "critical":
+            title = "No Security Issues Found"
+            body = "No Security Issues Found"
         else:
-            title = "No Security Issues Found."
-            body = "No Security Issues Found."
+            return None
 
-        payload = {"title": title, "body": body}
+        payload = {"title": title, "body": body, "labels": [vuln_data_payload["id"]]}
 
         r = requests.post(url, headers=headers, json=payload)
         if r.status_code == 201:
             print("Issue Created!")
         else:
             print(f"Failed to create issue. Status code: {r.status_code}")
+
+
+def delete_duplicate_issues_based_on_snyk_id():
+    github_repo = {"owner": "chevyphillip", "repo": "nodejs-goof"}
+    url = f"https://api.github.com/repos/{github_repo['owner']}/{github_repo['repo']}/issues"
+    headers = get_github_auth()
+
+    r = requests.get(url, headers=headers)
+    if r.status_code == 200:
+        issues = r.json()
+        snyk_ids = []
+        for issue in issues:
+            if issue["title"].startswith("Critical Vulnerability Found in"):
+                snyk_id = re.search(r"Snyk ID: ([a-z0-9]+)", issue["body"])
+                if snyk_id:
+                    snyk_ids.append(snyk_id.group(1))
+                else:
+                    print("No Snyk ID found in issue body.")
+        snyk_ids = list(set(snyk_ids))
+        for snyk_id in snyk_ids:
+            r = requests.get(
+                f"https://api.github.com/repos/{github_repo['owner']}/{github_repo['repo']}/issues?labels={snyk_id}",
+                headers=headers,
+            )
+            if r.status_code == 200:
+                issues = r.json()
+                for issue in issues:
+                    r = requests.delete
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from helper import *
-from issues import create_github_issue
+from issues import create_github_issue, delete_duplicate_issues_based_on_snyk_id
 
 
 def main():
+    delete_duplicate_issues_based_on_snyk_id()
     create_github_issue()
 
 
