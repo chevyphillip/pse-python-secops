4a75fd906a1ab7c98e74e1c40f47b124bec29342
Chevy Phillip <chevonmdphillip@gmail.com>
2023-11-11 15:30:38 -0500

    Refactor Github token retrieval and vulnerability parsing
    - Refactored the code to retrieve the Github auth token from environment variables exposed to the Github Action.
    - Updated the function for parsing vulnerabilities from Snyk to save them in separate JSON files for Node and Docker.
    - Implemented a check for critical vulnerabilities and created a Github issue if any were found. If no critical vulnerabilities were found, an issue stating that no security issues were found is created.

--- helper.py
+++ helper.py
@@ -1,6 +1,7 @@
 import os
 
 
+# Get the Github auth token from the environment variables that is exposed to the Github Action. Expires when a job is completed. Or within 24 hours.
 def get_github_auth():
     github_token = os.environ.get("GITHUB_TOKEN")
     headers = {
