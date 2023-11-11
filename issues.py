bbe325d9fa143c558b616812e0d21ae2766224e7
Chevy Phillip <chevonmdphillip@gmail.com>
2023-11-11 15:30:38 -0500

    Refactor Github token retrieval and vulnerability parsing
    - Refactored the code to retrieve the Github auth token from environment variables exposed to the Github Action.
    - Updated the function for parsing vulnerabilities from Snyk to save them in separate JSON files for Node and Docker.
    - Implemented a check for critical vulnerabilities and created a Github issue if any were found. If no critical vulnerabilities were found, an issue stating that no security issues were found is created.

--- issues.py
+++ issues.py
@@ -1,9 +1,9 @@
-from hmac import new
 import requests
 import json
 from helper import get_github_auth
 
 
+# Save the vulnerabilities from Snyk to a json file one for Node and one for Docker. Rutun errors if there are any.
 def parse_vuln_data():
     try:
         with open("node_vulns.json", "r") as f1, open("docker_vulns.json", "r") as f2:
@@ -16,17 +16,23 @@ def parse_vuln_data():
         return None
 
 
+# Create a Github issue if there are any critical vulnerabilities.
 def create_github_issue():
+
     github_repo = {"owner": "chevyphillip", "repo": "nodejs-goof"}
     url = f"https://api.github.com/repos/{github_repo['owner']}/{github_repo['repo']}/issues"
+
+    # Get the Github auth token from the helper function
     headers = get_github_auth()
 
+
+    # store the vulnerabilities in a variable called vuln_data
     vuln_data = parse_vuln_data()
 
-    # Remove duplicates from vuln_data
+    # Remove duplicates from vuln_data by converting it to a dictionary and back to a list. Dictionary keys are unique.
     vuln_data["vulnerabilities"] = list({vuln['id']:vuln for vuln in vuln_data["vulnerabilities"]}.values())
 
-    # After duplicates are removed, check if there are any critical vulnerabilities and create an issue
+    # After duplicates are removed, check if there are any critical vulnerabilities and create an issue if there are. If not, create an issue saying no security issues were found.
     for vuln_data_payload in vuln_data["vulnerabilities"]:
         if vuln_data_payload["severity"] == "critical":
             title = f"Critical Vulnerability Found in {vuln_data_payload['title']}"
